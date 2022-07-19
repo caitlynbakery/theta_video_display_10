2849c3cfc07da6c5d331d4c0b38d925e1b959b33
Caitlyn <caitlyn@oppkey.com>
2022-07-19 10:16:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 360 Video Display
+
+[async suspension](https://stackoverflow.com/questions/60658945/asynchronous-suspension-in-stacktrace-output-in-flutter)
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:theta_video_display_10/screens/dropbox_screen.dart';
+import 'package:theta_video_display_10/screens/firebase_screen.dart';
+import 'package:theta_video_display_10/screens/github_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: const MyApp(),
+    routes: <String, WidgetBuilder>{
+      "/firebase": (BuildContext context) => FireBaseScreen(),
+      "/dropbox": (BuildContext context) => DropBoxScreen(),
+      "/github": (BuildContext context) => GithubScreen(),
+    },
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("360 Video Storage")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/github");
+                },
+                icon: const FaIcon(
+                  FontAwesomeIcons.github,
+                  color: Color.fromARGB(255, 87, 87, 87),
+                ),
+                iconSize: 80,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/dropbox");
+                  },
+                  icon: const FaIcon(
+                    FontAwesomeIcons.dropbox,
+                    color: Color.fromARGB(255, 29, 137, 225),
+                  ),
+                  iconSize: 80),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/firebase");
+                  },
+                  icon: const FaIcon(
+                    FontAwesomeIcons.fire,
+                    color: Color.fromARGB(255, 255, 187, 86),
+                  ),
+                  iconSize: 80),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
